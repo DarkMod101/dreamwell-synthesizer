bd17819a01a36ac7aea154215f36be37b068e9bd
DarkMod101 <unfathomed101@gmail.com>
2026-06-30 10:33:36 -0400

    create piano body activated

--- app.js
+++ app.js
@@ -3553,7 +3553,7 @@ function createPianoNote(frequency) {
     createPianoStrings(ctx, frequency, now);
     // createPianoStringInteraction(ctx, frequency, now);
     // createPianoBridge(ctx, frequency, now, bridgeExcitation);
-    // createPianoBody(ctx, frequency, now, bodyExcitation);
+    createPianoBody(ctx, frequency, now, bodyExcitation);
     // createPianoCabinet(ctx, frequency, now);
     // createPianoSoundboard(ctx, frequency, now);
     // createPianoSympatheticResonance(ctx, frequency, now);
