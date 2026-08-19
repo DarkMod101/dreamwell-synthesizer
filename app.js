d5049bd449e0ca96d52cda98300c6ab4b3dd82a3
DarkMod101 <unfathomed101@gmail.com>
2026-08-19 06:18:38 -0400

    Test loop

--- app.js
+++ app.js
@@ -3340,7 +3340,7 @@ choirSampleGain = ctx.createGain();
 
     choirSampleSource.buffer = choirAhC3Buffer;
 
-    choirSampleSource.loop = true;
+    choirSampleSource.loop = false;
 
     choirSampleSource.loopStart = 3.18;
 choirSampleSource.loopEnd = 7.70;
